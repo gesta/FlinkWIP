349aa581f205f70587ab3b5cc5a46e93ad4dc7a7
Gerasim Stanchev <gero@tutuf.com>
2019-01-24 15:27:46 +0200

    Use ListState in lieu of ValueState

--- src/f22/CheckById.java
+++ src/f22/CheckById.java
@@ -1,19 +1,26 @@
 package f22;
 
-import java.util.List;
 import org.apache.flink.api.common.functions.RichMapFunction;
-import org.apache.flink.api.common.state.ValueState;
+import java.util.List;
+import org.apache.flink.api.common.state.ListState;
+import org.apache.flink.api.common.state.ListStateDescriptor;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.configuration.Configuration;
 
 public class CheckById extends RichMapFunction<String, List<String>> {
-	private transient ValueState<List<String>> sum;
+	private static final long serialVersionUID = 1L;
+	private ListState<String> sum;
 
-    @Override
+	@Override
     public List<String> map(String input) throws Exception {
-    	// take the current state
-    	List<String> current = sum.value();
-        current.add(input);
-        // update the state
-        sum.update(current);
-        return current;
+    	sum.add(input);
+    	return (List<String>) sum.get();
+    }
+
+	public void open(Configuration cfg) {
+		ListStateDescriptor<String> listState = new ListStateDescriptor<String>("state", String.class);
+		System.out.println(listState);
+        sum = getRuntimeContext().getListState(listState);
     }
 }
